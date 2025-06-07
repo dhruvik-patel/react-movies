a048d93089af8606b686b75538ea5f450f95c1de
Dhruvik Patel <dhruvik777@gmail.com>
2025-06-07 18:34:13 -0400

    worked on header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Search from './components/Search'
 
 const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState('')
+
   return (
     <main>
       <div className="pattern"></div>
@@ -11,7 +14,8 @@ const App = () => {
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hasssle</h1>
         </header>
 
-        <Search/>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className="text-white">{searchTerm}</h1>
       </div>
     </main>
   )
